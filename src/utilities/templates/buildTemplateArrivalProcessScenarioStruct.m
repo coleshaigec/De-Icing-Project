3c0b7162f30f07b0c8ec579f5fb90a0f4d09b478
Cole Shaigec <coleshaigec@crc-dot1x-nat-10-239-166-37.bu.edu>
2026-04-28 14:24:51 -0400

    implement template builder for arrival process scenarios

---
+++ src/utilities/templates/buildTemplateArrivalProcessScenarioStruct.m
@@ -0,0 +1,27 @@
+function templateArrivalProcessScenarioStruct = buildTemplateArrivalProcessScenarioStruct()
+    % BUILDTEMPLATEARRIVALPROCESSSCENARIOSTRUCT Builds template arrival process scenario container for preallocation.
+    %
+    % OUTPUT
+    %  templateArrivalProcessScenarioStruct struct with fields
+    %      .scenarioName
+    %      .lambdaBase
+    %      .lambdaPeak
+    %      .t0
+    %      .sigmaLambda
+    %      .Ca
+
+    templateArrivalProcessScenarioStruct = struct();
+
+    templateArrivalProcessScenarioStruct.scenarioName = [];
+    templateArrivalProcessScenarioStruct.lambdaBase = [];
+    templateArrivalProcessScenarioStruct.lambdaPeak = [];
+    templateArrivalProcessScenarioStruct.t0 = [];
+    templateArrivalProcessScenarioStruct.sigmaLambda =[];
+    templateArrivalProcessScenarioStruct.Ca = [];
+end
+    
+    
+    
+    
+    
+    
