008b4ef7d5633461ba4a4f9adcbede9379c41b2e
Cole Shaigec <coleshaigec@crc-dot1x-nat-10-239-73-95.bu.edu>
2026-05-02 19:40:24 -0400

    Implement skeleton taxi/takeoff queue

---
+++ src/config/getNumberOfTaxiTakeoffServers.m
@@ -0,0 +1,5 @@
+function numServers = getNumberOfTaxiTakeoffServers()
+    % GETNUMBEROFRUNWAYS Allows user to configure number of servers for G/G/k taxi/takeoff service model.
+
+    numServers = 2;
+end
